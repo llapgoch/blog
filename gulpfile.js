e7c55e2b331a3a76ebcc9f5fcdccb965b4bcf865
Dave Baker <dbaker@iweb.co.uk>
2016-08-15 14:30:08 +0100

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-
-gulp.task('hello', function() {
-  console.log('Hello Zell');
-});
-
-gulp.task('sass', function(){
+gulp.task('default', function(){
   return gulp.src('library/scss/style.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('library/css'))
 });
+
+gulp.watch('library/scss/**/*.scss', ['default']);
